943d2d6fec26c27f0cc5529ed7b4616bbfdaa6e9
chlorocloud-collab <chlorocloud@gmail.com>
2026-06-22 00:00:06 +0800

    Selesai Bagian 3: Multi-Class

---
+++ src/bagian3/kontak/MainKontak.java
@@ -0,0 +1,25 @@
+package bagian3.kontak;
+
+public class MainKontak {
+    public static void main(String[] args) {
+        BukuKontak buku = new BukuKontak("kontak.txt");
+        buku.tambahKontak(new Kontak("Andi", "0811111", "andi@mail.com"));
+        buku.tambahKontak(new Kontak("Budi", "0822222", "budi@mail.com"));
+        buku.tambahKontak(new Kontak("Citra", "0833333", "citra@mail.com"));
+        buku.tampilkanSemua();
+        buku.simpanKeBerkas();
+
+        System.out.println();
+
+        // Hapus kontak
+        buku.hapusKontak("Budi");
+        buku.tampilkanSemua();
+
+        System.out.println();
+
+        BukuKontak bukuLain = new BukuKontak("kontak.txt");
+        bukuLain.muatDariBerkas();
+        bukuLain.tampilkanSemua();
+        System.out.println("Jumlah kontak: " + bukuLain.jumlahKontak());
+    }
+}
